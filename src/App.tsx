60e27d1393b245f0e782196c21a69fff6bae59ec
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2025-01-31 21:22:51 -0500

    React-Tailwindcss Components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-center text-5xl mt-3">
+        {" "}
+        Welcome to React + Tailwindcss Template{" "}
+      </h2>
+    </>
+  );
+}
+
+export default App;
