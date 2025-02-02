67bda7f4ea9c32a038158dcc22e1d513b8c12265
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2025-02-01 23:52:18 -0500

    Added Dynamic Charts

---
+++ src/assets/data/ChartData.ts
@@ -0,0 +1,24 @@
+export const PiChartData = [
+  { name: "Sun", uv: 4000, pv: 2400, amt: 2400 },
+  { name: "Mon", uv: 3000, pv: 1398, amt: 2210 },
+  { name: "Tue", uv: 2000, pv: 9800, amt: 2290 },
+  { name: "Wed", uv: 2780, pv: 3908, amt: 2000 },
+  { name: "Thu", uv: 1890, pv: 4800, amt: 2181 },
+  { name: "Fri", uv: 2390, pv: 3800, amt: 2500 },
+  { name: "Sat", uv: 3490, pv: 4300, amt: 2100 },
+];
+
+export const BarChartData = [
+  { name: "john", uv: 4000, pv: 2400, amt: 2400 },
+  { name: "jane", uv: 3000, pv: 1398, amt: 2210 },
+  { name: "joe", uv: 2000, pv: 9800, amt: 2290 },
+  { name: "jill", uv: 2780, pv: 3908, amt: 2000 },
+  { name: "jane", uv: 1890, pv: 4800, amt: 2181 },
+  { name: "joe", uv: 2390, pv: 3800, amt: 2500 },
+  { name: "jill", uv: 3490, pv: 4300, amt: 2100 },
+  { name: "sara", uv: 1490, pv: 2300, amt: 1100 },
+  { name: "janes", uv: 3490, pv: 4300, amt: 2100 },
+  { name: "jills", uv: 3490, pv: 4300, amt: 2100 },
+  { name: "janes", uv: 3490, pv: 4300, amt: 2100 },
+  { name: "jills", uv: 3490, pv: 4300, amt: 2100 },
+];
--- src/pages/Help.tsx
+++
@@ -1,5 +0,0 @@
-const Help = () => {
-  return <div>Welcome to Help Page</div>;
-};
-
-export default Help;
